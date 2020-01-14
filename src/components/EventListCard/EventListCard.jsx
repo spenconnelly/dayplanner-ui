ce7d37074437f280976b90a98630b30d04298ae0
spencerconnelly <spenconnelly@gmail.com>
2020-01-14 13:29:18 -0500

    more card styling.

--- src/components/EventListCard/EventListCard.jsx
+++ src/components/EventListCard/EventListCard.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import dateFormat from 'dateformat';
 import './EventListCard.scss';
 
-const EventListCard = ({ id, name, date, description }) => (
-    <div className="card--container">
-        <div className="header--block">
-            {name}
+const EventListCard = ({ id, name, date, description }) => {
+    return (
+        <div className="card--container">
+            <div className="header--block">
+                <div>{name}</div>
+                <div>{dateFormat(Date(date), 'h:MM:ss TT dddd, mmmm dS, yyyy')}</div>
+            </div>
+            <div className="body--block">
+                {description}
+            </div>
         </div>
-        <div className="body--block">
-            {date}
-        </div>
-    </div>
-);
+    );
+};
 
 export default EventListCard;
